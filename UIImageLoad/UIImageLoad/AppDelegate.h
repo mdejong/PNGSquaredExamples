7fb4f9b6a779d921af814806884ef0dc442fa280
Mo DeJong <mo@modejong.com>
2016-10-16 15:00:27 -0700

    import empty project named UIImageLoad

---
+++ UIImageLoad/UIImageLoad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIImageLoad
+//
+//  Created by Mo DeJong on 10/16/16.
+//  Copyright © 2016 HelpURock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIImageLoad/UIImageLoad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIImageLoad
+//
+//  Created by Mo DeJong on 10/16/16.
+//  Copyright © 2016 HelpURock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
