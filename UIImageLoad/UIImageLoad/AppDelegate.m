6168762a81a6512b703ea405278095ce725c13b8
Mo DeJong <mo@modejong.com>
2016-10-16 15:49:30 -0700

    add PNGSQUARED define in UIImageLoad build target, add import statements so that UIImageLoad2 target compiles and links using modules

--- UIImageLoad/UIImageLoad/AppDelegate.m
+++ UIImageLoad/UIImageLoad/AppDelegate.m
@@ -8,7 +8,24 @@
 
 #import "AppDelegate.h"
 
+#if defined(PNGSQUARED)
+@import PNGSquared;
+@import CoreGraphics;
+@import ImageIO;
+@import QuartzCore;
+@import MobileCoreServices;
+#import "UIImage+PNGSquared.h"
+#endif // PNGSQUARED
+
 @interface AppDelegate ()
+
+#if defined(PNGSQUARED)
+// A ref counted dictionary is required to hold active refs to UIImage objects
+// cached by the custom UIImage overloaded methods.
+
+@property (nonatomic, retain) NSMutableDictionary *imageCache;
+
+#endif // PNGSQUARED
 
 @end
 
@@ -17,8 +34,45 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   // Override point for customization after application launch.
+  
+#if defined(PNGSQUARED)
+  // Invoke setup method to swap in custom impl of [UIImage imageNamed:]
+  
+  self.imageCache = [NSMutableDictionary dictionary];
+  [UIImage setupAppInstance:self.imageCache];
+  
+  [[NSNotificationCenter defaultCenter] addObserver:self
+                                           selector:@selector(allReadyNotification:)
+                                               name:UIImagePNGSquaredAllReadyNotification
+                                             object:nil];
+#else
+  // nop
+#endif // PNGSQUARED
+  
   return YES;
 }
+
+#if defined(PNGSQUARED)
+
+// This notification is delivered when the main bundle has been scanned and normal
+// PNG image loading from .png2 sources is ready to begin.
+
+- (void) allReadyNotification:(NSNotification*)notification
+{
+#if defined(DEBUG)
+  NSLog(@"allReadyNotification");
+#endif // DEBUG
+  
+  [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                  name:UIImagePNGSquaredAllReadyNotification
+                                                object:nil];
+  
+  // FIXME: Invoke method to signal the view controller that image can now be loaded
+  
+  return;
+}
+
+#endif // PNGSQUARED
 
 - (void)applicationWillResignActive:(UIApplication *)application {
   // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
