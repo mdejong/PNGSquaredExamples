c2f342d4122dd5b654744de9603276ceed810f17
Mo DeJong <mo@modejong.com>
2016-10-20 12:34:06 -0700

    call UIImage overloaded method from SwiftScale ViewController.swift, currently loads 1x version

--- SwiftScale/SwiftScale/ViewController.swift
+++ SwiftScale/SwiftScale/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var imageView : UIImageView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    assert(self.imageView != nil)
+
+#if PNGSQUARED
+    NSNotificationCenter.defaultCenter().addObserver(self,
+      selector: #selector(allReadyNotification),
+      name: UIImagePNGSquaredAllReadyNotification,
+      object: nil)
+#endif
   }
 
+  func allReadyNotification()
+  {
+#if PNGSQUARED
+    let image = UIImage(named: "one")
+    self.imageView.image = image
+#endif
+    return;
+  }
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
