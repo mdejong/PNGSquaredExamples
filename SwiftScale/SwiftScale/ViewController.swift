ec65b7d490c85052011ac203491f21f34d3cd9b9
Mo DeJong <mo@modejong.com>
2019-05-18 17:45:19 -0700

    recreate SwiftScale example by copying in files from SwiftScaleOld

--- SwiftScale/SwiftScale/ViewController.swift
+++ SwiftScale/SwiftScale/ViewController.swift
@@ -2,17 +2,44 @@
 //  ViewController.swift
 //  SwiftScale
 //
-//  Created by Mo DeJong on 5/18/19.
-//  Copyright © 2019 HelpURock. All rights reserved.
+//  Created by Mo DeJong on 10/20/16.
+//  Copyright © 2016 HelpURock. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var imageView : UIImageView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    // Do any additional setup after loading the view, typically from a nib.
+    assert(self.imageView != nil)
+
+#if PNGSQUARED
+    let nc = NotificationCenter.default
+    nc.addObserver(forName:Notification.Name(rawValue:UIImagePNGSquaredAllReadyNotification),
+                   object:nil,
+                   queue:nil) {
+                    notification in
+                    self.allReadyNotification()
+  }
+#endif
+  }
+
+  func allReadyNotification()
+  {
+#if PNGSQUARED
+    let image = UIImage(named: "one")
+    self.imageView.image = image
+#endif
+    return;
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
   }
 
 
